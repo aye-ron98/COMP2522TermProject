f2ad0862d9635db81eff7266b318924f1d0db5d3
Alexandra Wong <lex115wong@gmail.com>
2023-03-23 15:44:49 -0700

    Start Character Class

---
+++ .idea/src/Character.java
@@ -0,0 +1,12 @@
+public class Character {
+    int STARTING_HAND_LIMIT = 5;
+
+    private int health;
+    private int agility;
+    private int defence;
+    private int handLimit;
+//    private enum Type;
+//    private Card[];
+    String name;
+    private int xp;
+}
