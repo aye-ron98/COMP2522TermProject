a345e207c2e20e8f69b5a009245ee4a91c34457a
aye-ron98 <aron.zhang013@gmail.com>
2023-03-26 23:55:20 -0700

    add description fields to cards

--- bcit/Card.java
+++ bcit/Card.java
@@ -10,22 +10,25 @@ public class Card {
     private final int value;
     private String name;
 
-    private Card(TYPE t, ACTION a, int d, String n) {
+    private String description;
+
+    private Card(TYPE t, ACTION a, int d, String n, String des) {
         this.type = t;
         this.action = a;
         this.value = d;
         this.name = n;
+        this.description = des;
     }
 
     public static Card constructNewCard() {
         int generate = rand.nextInt(0, 6);
         return switch (generate) {
-            case 0 -> new Card(TYPE.AIR, ACTION.ATTACK, 5, "Fart");
-            case 1 -> new Card(TYPE.FIRE, ACTION.ATTACK, 10, "DrakeSpecial");
-            case 2 -> new Card(TYPE.EARTH, ACTION.ATTACK, 3, "Ouch");
-            case 3 -> new Card(TYPE.WATER, ACTION.ATTACK, 7, "SuperSoaker");
-            case 4 -> new Card(TYPE.AIR, ACTION.DEFEND, -5, "BlowAwau");
-            case 5 -> new Card(TYPE.FIRE, ACTION.HEAL, 10, "KarateKidSpecial???");
+            case 0 -> new Card(TYPE.AIR, ACTION.ATTACK, 5, "Fart", "an attack");
+            case 1 -> new Card(TYPE.FIRE, ACTION.ATTACK, 10, "DrakeSpecial", "an attack");
+            case 2 -> new Card(TYPE.EARTH, ACTION.ATTACK, 3, "Ouch", "an attack");
+            case 3 -> new Card(TYPE.WATER, ACTION.ATTACK, 7, "SuperSoaker", "an attack");
+            case 4 -> new Card(TYPE.AIR, ACTION.DEFEND, -5, "BlowAwau", "a defend");
+            case 5 -> new Card(TYPE.FIRE, ACTION.HEAL, 10, "KarateKidSpecial???", "heal move");
             default -> null;
         };
     }
@@ -39,6 +42,10 @@ public class Card {
 
     public int getValue() {
         return value;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     @Override
